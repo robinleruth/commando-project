9c0beae765659fa2ea4aeb9058fa6c8b7b49f33f
robinleruth <robin.leruth@gmail.com>
2020-03-29 20:55:25 -0400

    ui

--- app/infrastructure/data/gspc_file_connector.py
+++ app/infrastructure/data/gspc_file_connector.py
@@ -11,6 +11,6 @@ class GspcFileConnector(DataConnector):
         df = pd.read_csv(os.path.join(file_path, 'sp500.csv'))
         df[app_config.AS_OF_DATE] = pd.to_datetime(df[app_config.AS_OF_DATE])
         df[app_config.VOLATILITY] = (df[app_config.HIGH] - df[app_config.LOW]) / df[app_config.LOW]
-        df[app_config.TURNOVER] = 0
+        df[app_config.TURNOVER] = df['Volume']
         df[app_config.SPOT] = df[app_config.CLOSE]
         return df
