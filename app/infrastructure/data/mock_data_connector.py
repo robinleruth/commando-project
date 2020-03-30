44387ceebbf0fc593aa6619cd3c02b4e37c82ed2
robinleruth <robin.leruth@gmail.com>
2020-03-29 20:27:36 -0400

    fix mock value

--- app/infrastructure/data/mock_data_connector.py
+++ app/infrastructure/data/mock_data_connector.py
@@ -10,11 +10,11 @@ class MockDataConnector(DataConnector):
     def get_df(self) -> pd.DataFrame:
         dates = ['2020-03-{}'.format(str(i)) for i in range(1, 31)]
         dates = list(map(lambda x: dt.datetime.strptime(x, "%Y-%m-%d"), dates))
-        spot = [random.randint(80, 120) for _ in range(1, 31)]
-        open = [random.randint(80, 120) for _ in range(1, 31)]
+        spot = [random.randint(50, 200) for _ in range(1, 31)]
         high = [random.randint(80, 120) for _ in range(1, 31)]
-        close = [random.randint(80, 120) for _ in range(1, 31)]
-        low = [random.randint(80, 120) for _ in range(1, 31)]
+        low = [round(i * (1 - random.random())) for i in high]
+        open = [random.randint(i[1], i[0]) for i in list(zip(high, low))]
+        close = [random.randint(i[1], i[0]) for i in list(zip(high, low))]
         vol = [random.randint(80, 120) for _ in range(1, 31)]
         df = pd.DataFrame(list(zip(dates, spot, open, high, low, close, vol)),
                           columns=[app_config.AS_OF_DATE, app_config.SPOT,
